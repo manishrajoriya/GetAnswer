97b7a65b6a14443c0040815201f18794b1b63e7d
manish <gkrajoriya3@gmail.com>
2025-02-15 16:54:02 +0530

    answer

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,34 +6,20 @@ import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+    
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index"  />
+        <Stack.Screen name="home"  />
       </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+      
+    
   );
 }
---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Index = () => {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({})
+
+export default Index;
