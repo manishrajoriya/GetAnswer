c3ca3679952ea3b630454a4dad97cebff2c4759b
manish <gkrajoriya3@gmail.com>
2025-02-17 10:16:45 +0530

    build

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { View, Text } from 'react-native'
+import React from 'react'
 
-const Index = () => {
+const index = () => {
   return (
     <View>
-      <Text>index hello</Text>
+      <Text>index</Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({})
-
-export default Index;
+export default index
